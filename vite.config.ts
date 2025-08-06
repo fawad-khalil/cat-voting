57c7f7d8e4fc84f30a8a37b7d6737168792f4feb
Fawad Khalil <iam.fawad.khalil@outlook.com>
2025-08-07 00:33:52 +0500

    fix: base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'cat-voting.github.io',
+  base: '/cat-voting/',
   test: {
     globals: true,
     environment: 'jsdom',
